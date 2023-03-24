6f497a92bf21f815a924815ed9bcd96ceab57219
Owen de Jesus <owendanieldejesuslopez70@gmail.com>
2023-03-23 22:44:35 -0600

    Creando conexion a base de datos

---
+++ config/global.php
@@ -0,0 +1,19 @@
+<?php 
+define("DB_HOST","localhost");
+
+//Nombre de la base de datos
+define("DB_NAME", "tiendasofstore");
+
+//Usuario de la base de datos
+define("DB_USERNAME", "root");
+
+//Contraseña del usuario de la base de datos
+define("DB_PASSWORD", "");
+
+//definimos la codificaci贸n de los caracteres
+define("DB_ENCODE","utf8");
+
+//Definimos una constante como nombre del proyecto
+define("PRO_NOMBRE","Prueba de la base de datos");
+
+?>
